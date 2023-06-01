af34868d51d1f0a5825f3f27cdfc0babf9546238
Therenegadee <melkindaniil@yandex.ru>
2023-06-01 19:41:43 +0300

    Fixed Errors

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,8 +9,11 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final int amountOfRows = 4;
-    private static final Scanner sc = new Scanner(System.in);
+    private static final int AMOUNT_OF_ROWS = 4;
+    private static final String CHOOSE_FILE_FORMAT = "Введите формат файла, из которого вы хотите прочитать ссылки: .csv или .xlsx";
+    private static final String CHOOSE_FILE_PATH_IN = "Введите путь к файлу, из которого хотите читать ссылки: ";
+    private static final String CHOOSE_FILE_PATH_OUT = "Введите путь к файлу вывода: ";
+    private static final Scanner SC = new Scanner(System.in);
     private static final Map<Integer, String> HEADER_DATA = new LinkedHashMap<>(Map.of(
             0, "date_start",
             1, "date_end",
@@ -21,28 +24,31 @@ public class Main {
     ));
 
     public static void main(String[] args) throws IOException {
-        System.out.println("Введите формат файла, из которого вы хотите прочитать ссылки: .csv или .xlsx");
-        String chooseFormat = sc.next();
+        System.out.println(CHOOSE_FILE_FORMAT);
+        String chooseFormat = SC.next();
         if (chooseFormat.equals(".csv")) {
-            System.out.println("Введите путь к файлу, из которого хотите читать ссылки: ");
-            File inputFile = new File(sc.next());
+            System.out.println(CHOOSE_FILE_PATH_IN);
+            File inputFile = new File(SC.next());
             CsvImport csvImport = new CsvImport();
-            CsvExport csvExport= new CsvExport();
+            CsvExport csvExport = new CsvExport();
             var data = csvImport.parseCSV(inputFile);
-            System.out.println("Введите путь к файлу вывода: ");
-            csvExport.saveCSV(data, HEADER_DATA, sc.next());
+            System.out.println(CHOOSE_FILE_PATH_OUT);
+            csvExport.saveCSV(data, HEADER_DATA, SC.next());
         } else if (chooseFormat.equals(".xlsx")) {
-            System.out.println("Введите путь к файлу, из которого хотите читать ссылки: ");
-            File file = new File(sc.next());
+            System.out.println(CHOOSE_FILE_PATH_IN);
+            File file = new File(SC.next());
             ExcelImport excelImport = new ExcelImport();
             ExcelExport excelExport = new ExcelExport(HEADER_DATA);
-            var data = excelImport.readExcel(file, amountOfRows);
-            System.out.println("Введите путь к файлу вывода: ");
-            excelExport.exportData(data, sc.next());
+            var data = excelImport.readExcel(file, AMOUNT_OF_ROWS);
+            System.out.println(CHOOSE_FILE_PATH_OUT);
+            excelExport.exportData(data, SC.next());
+//        } else if (chooseFormat.equals(".doc")) {
+//            System.out.println(CHOOSE_FILE_PATH_IN);
+//            File file = new File(SC.next());
+//            System.out.println(CHOOSE_FILE_PATH_OUT);
         } else {
-            System.err.println("ошибка");
+            System.err.println("Ошибка");
         }
     }
-
 }
 
