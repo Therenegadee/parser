4ce22b4c498f7de5bf969e46498005e100464fc9
Therenegadee <melkindaniil@yandex.ru>
2023-06-03 14:12:09 +0300

    Changes in 'main' method, add separate methods for Parse and Export

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    private static final String CHOOSE_FILE_FORMAT = "Введите формат файла, из которого вы хотите прочитать ссылки: .csv или .xlsx";
+    private static final String CHOOSE_INPUT_FILE_FORMAT = "Введите формат файла, из которого вы хотите прочитать ссылки: .csv или .xlsx";
+    private static final String CHOOSE_OUTPUT_FILE_FORMAT = "Введите формат файла вывода: .csv или .xlsx";
     private static final String CHOOSE_FILE_PATH_IN = "Введите путь к файлу, из которого хотите читать ссылки: ";
     private static final String CHOOSE_FILE_PATH_OUT = "Введите путь к файлу вывода: ";
     private static final Scanner SC = new Scanner(System.in);
@@ -22,31 +23,55 @@ public class Main {
     );
 
     public static void main(String[] args) throws IOException {
-        System.out.println(CHOOSE_FILE_FORMAT);
-        String chooseFormat = SC.next();
-        switch (chooseFormat) {
+        System.out.println(CHOOSE_INPUT_FILE_FORMAT);
+        String chooseFormatInput = SC.next();
+        List<Info> parsedData = switch (chooseFormatInput) {
             case ".csv" -> csvParse();
             case ".xlsx" -> excelParse();
+            /*case ".docx" -> {
+            }*/
+            default -> throw new IllegalStateException("Unexpected value: " + chooseFormatInput);
+        };
+        System.out.println(CHOOSE_OUTPUT_FILE_FORMAT);
+        String chooseFormatOutput = SC.next();
+        switch (chooseFormatOutput) {
+            case ".csv" -> csvExport(parsedData);
+            case ".xlsx" -> excelExport(parsedData);
+            default -> throw new IllegalStateException("Unexpected value: " + chooseFormatOutput);
         }
     }
 
-    public static void csvParse () throws IOException {
+    public static List<Info> csvParse() {
         System.out.println(CHOOSE_FILE_PATH_IN);
         File inputFile = new File(SC.next());
         CsvParser csvParser = new CsvParser();
-        CsvExporter csvExporter = new CsvExporter();
         var data = csvParser.parseCSV(inputFile);
+        return data;
+    }
+
+    public static void csvExport(List<Info> data) throws IOException {
+        CsvExporter csvExporter = new CsvExporter();
         System.out.println(CHOOSE_FILE_PATH_OUT);
         csvExporter.saveCSV(data, HEADER_DATA, SC.next());
     }
-    public static void excelParse () throws IOException {
+
+
+    public static List<Info> excelParse() throws IOException {
         System.out.println(CHOOSE_FILE_PATH_IN);
         File file = new File(SC.next());
         ExcelParser excelParser = new ExcelParser();
-        ExcelExporter excelExporter = new ExcelExporter(HEADER_DATA);
         var data = excelParser.parseExcel(file);
+        return data;
+    }
+
+    public static void excelExport (List<Info> data) {
+        ExcelExporter excelExporter = new ExcelExporter(HEADER_DATA);
         System.out.println(CHOOSE_FILE_PATH_OUT);
         excelExporter.exportData(data, SC.next());
     }
+
+    /*public static void docxParse() {
+
+    }*/
 }
 
